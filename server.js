c6d71aa60031643f598366bbc7d619e610ddf0c8
DidenkoNikita <nikitadidenko2@gmail.com>
2023-02-13 13:24:51 +0300

    two commit

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const app = require('express')()
+const app = require('express')();
+const cors = require('cors');
 
-const host = '127.0.0.1'
-const port = 7000
+const data = [{id: 'b1', title: "Board1", tasks: [{idT: 1, completed: false, titleT: "task1"}, {idT: 2, completed: true, titleT: "task2"}, {idT: 3, completed: false, titleT: "task3"}]},
+              {id: 'b2', title: "Board2", tasks: [{idT: 4, completed: false, titleT: "task4"}, {idT: 5, completed: true, titleT: "task5"}, {idT: 6, completed: false, titleT: "task6"}]}];
+
+
+const host = '127.0.0.1';
+const port = 7000;
+
+app.use(cors());
 
 app.get('/boards', (req, res) => {
   // res.status(200).type('text/plain')
-  res.status(200).json({title: 'dont give a fuck'});
+  res.status(200).json(data);
 })
 
 app.use((req, res, next) => {
