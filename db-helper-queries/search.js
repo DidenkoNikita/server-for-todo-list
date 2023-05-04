17625ae67bcd07acf8d59fc44c6d647e197f56df
DidenkoNikita <n.didenko@redlight.group>
2023-05-04 13:14:23 +0300

    just in case

--- db-helper-queries/search.js
+++ db-helper-queries/search.js
@@ -2,34 +2,34 @@ import prisma from "../prisma/prismaClient.mjs";
 
 export async function userSearch(login) {
   try {
-      const data = await prisma.users.findUnique({
-        where: {
-          login: login
-        },
-        select: {
-          id: true
-        }
-      })
-      return data;
-    } catch(e) {
-      return e;
-    }
+    const data = await prisma.users.findUnique({
+      where: {
+        login: login
+      },
+      select: {
+        id: true
+      }
+    })
+    return data;
+  } catch(e) {
+    return e;
+  }
 }
 
 export async function boardSearch(idUser) {
   try {
-      const data = await prisma.boards.findFirst({
-        where: {
-          user_id: idUser
-        },
-        select: {
-          id: true
-        }
-      })
-      return data;
-    } catch(e) {
-      return e;
-    }
+    const data = await prisma.boards.findFirst({
+      where: {
+        user_id: idUser
+      },
+      select: {
+        id: true
+      }
+    })
+    return data;
+  } catch(e) {
+    return e;
+  }
 }
 
 export async function idSearch(accessToken) {
